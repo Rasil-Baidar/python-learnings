565ee17d8aad1f8d12c0c611a0204bfebcf12ba9
Rasil Baidar <rasil.baidar@docorto.ai>
2026-09-09 14:31:14 +1000

    multiplication table using rnage

--- lessons/dataTypes/lessonsRange.py
+++ lessons/dataTypes/lessonsRange.py
@@ -4,3 +4,7 @@ def lesson_range():
     print(f"range(1,10) : {list(range(1,10))}")
     print(f"range(1,10,2) : {list(range(1,10,2))}")
     print(f"Even numbers using range:{list(range(2,21,2))}")
+    print(f"--------------------------------\n")
+    print(f"Multiplication table of 5 using range:")
+    for num in range(0,11):
+        print(f"5 * {num}: {5 * num}")
